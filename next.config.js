6e0d0b4e30d6336116eb1b1c9d90cde39aaf48f7
Ionut Nohai <nohai.ionut02@gmail.com>
2023-03-25 21:38:12 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'my-app',
+          remotes: {
+            checkout: "checkout@https://nextjs-mfe-f3bz.vercel.app/_next/static/chunks/remoteEntry.js",
+            mars_weather: 'mars_weather@https://mf-remote.netlify.app/remoteEntry.js',
+          },
+          filename: 'static/chunks/remoteEntry.js',
+          shared: {},
+        }),
+      );
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
+
